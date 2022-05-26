d5b47c64624e421ef507f4ac6bf8f56544964dca
kate <katarzynadudziak94@gmail.com>
2022-05-26 12:29:54 +0200

    Main view and history view are parametrized by messages template

--- chat/urls.py
+++ chat/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.chat_view, name='main'),
     path('delete/', views.delete_message, name='delete'),
+    path('history/', views.history, name='history'),
 ]
--- chat/views.py
+++ chat/views.py
@@ -32,3 +32,19 @@ def delete_message(request):
             return HttpResponseNotFound('404')
     
     return HttpResponseRedirect('/')
+
+
+def history(request):
+    if request.method == 'POST':
+        form = MessageForm(request.POST)
+
+        if form.is_valid():
+            content = form.cleaned_data['content']
+            message = MessageModel(content=content, date=timezone.now(), author=request.user)
+            message.save()
+            return HttpResponseRedirect('/')
+
+    message = MessageModel.objects.all().order_by('-date')
+
+    
+    return render(request, 'history.html', {'messages' : message})
