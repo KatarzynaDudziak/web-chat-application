2144afb3d00b86445e327ef9c54af3279ec07ea1
kate <katarzynadudziak94@gmail.com>
2022-05-26 15:19:42 +0200

    PD4.3 Added pagination system

--- chat/views.py
+++ chat/views.py
@@ -2,6 +2,7 @@ from django.utils import timezone
 from django.http import HttpResponseNotFound
 from django.http.response import HttpResponseRedirect
 from django.shortcuts import render
+from django.core.paginator import Paginator
 from .models import *
 from .forms import *
 
@@ -19,9 +20,9 @@ def chat_view(request):
             message.save()
             return HttpResponseRedirect('/')
 
-    message = MessageModel.objects.all().order_by('-date')
+    messages_list = MessageModel.objects.all().order_by('-date')
 
-    return render(request, 'main.html', {'messages' : message})
+    return render(request, 'main.html', {'messages' : messages_list})
 
 
 def delete_message(request):
@@ -35,16 +36,12 @@ def delete_message(request):
 
 
 def history(request):
-    if request.method == 'POST':
-        form = MessageForm(request.POST)
+    messages_list = MessageModel.objects.all().order_by('-date')
 
-        if form.is_valid():
-            content = form.cleaned_data['content']
-            message = MessageModel(content=content, date=timezone.now(), author=request.user)
-            message.save()
-            return HttpResponseRedirect('/')
-
-    message = MessageModel.objects.all().order_by('-date')
+    p = Paginator(messages_list, 5)
+    page = request.GET.get('page')
+    messages = p.get_page(page)
+    nums = "a" * messages.paginator.num_pages
 
     
-    return render(request, 'history.html', {'messages' : message})
+    return render(request, 'history.html', {'messages' : messages, 'number_of_pages' : nums})
